b0fb244dbea879a995986b7b559ae1844e94a19a
jiyong0419 <82662371+jiyong0419@users.noreply.github.com>
2021-10-18 10:43:32 +0900

    #3.1 HTML in JS

--- app.js
+++ app.js
@@ -1,12 +1,6 @@
-const age = parseInt(prompt("Your age?"));
+const title = document.getElementById("title")
 
+title.innerText = "Got you!"
 
-if (isNaN(age)) {
-    console.log("Please write number");
-}else if(age<18){
-    console.log("You can't buy this");
-}else if(age<50){
-    console.log("You can buy this");
-}else {
-    console.log("Maybe you must stop to drink this");
-}
+console.log(title.id);
+console.log(title.className);
