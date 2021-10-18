77ba7373122836f7af3840cb6bd6368bba63b983
jiyong0419 <82662371+jiyong0419@users.noreply.github.com>
2021-10-18 12:17:29 +0900

    #3.4 Events part Two

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const firstTitle = document.querySelector(".hello h1")
-firstTitle.addEventListener("click",handleTitleClick)
 
 function handleTitleClick(){
     firstTitle.style.color="blue"
 }
+function handleMouseEnter(){
+    firstTitle.innerText="Mouse is here!"
+}
+function handleMouseLeave(){
+    firstTitle.innerText="Mouse is gone !"
+}
+
+firstTitle.addEventListener("click",handleTitleClick)
+firstTitle.addEventListener("mouseenter",handleMouseEnter)
+firstTitle.addEventListener("mouseleave",handleMouseLeave)
+
 console.dir(firstTitle)
