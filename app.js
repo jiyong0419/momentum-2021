5d72b4aeecd2691862de71c5c5b38454659373ba
jiyong0419 <82662371+jiyong0419@users.noreply.github.com>
2021-10-18 18:39:10 +0900

    #3.7 CSS in JS part Two

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 const h1 = document.querySelector(".hello h1")
 
 function handleTitleClick(){
-    const currentColor = h1.style.color;
-    let newColor
-    if (currentColor==="blue") {
-        newColor="tomato"
-    }else{
-        newColor="blue"
-    }
-    h1.style.color = newColor
+    const clickedClass="active"
+    if (h1.className===clickedClass){
+      h1.className="";
+    }else {
+      h1.className=clickedClass;
+  }
 }
 
 h1.addEventListener("click",handleTitleClick)
